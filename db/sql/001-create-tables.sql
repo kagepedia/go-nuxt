04375721bed9cd0a2b7a907ca337a94f98c3dcdc
Kage Soichiro <k4gepedia@gmail.com>
2020-05-06 02:13:36 +0900

    docker initial setting fix

--- db/sql/001-create-tables.sql
+++ db/sql/001-create-tables.sql
@@ -1,23 +1,5 @@
-DROP DATABASE IF EXISTS sample_db;
 CREATE DATABASE sample_db;
 USE sample_db;
-
----- drop ----
-DROP TABLE IF EXISTS `t_user`;
-
----- create ----
-create table IF not exists `t_user`
-(
- `id`               INT(11) AUTO_INCREMENT,
- `name`             VARCHAR(64) NOT NULL,
- `email`            VARCHAR(256) NOT NULL,
- `created_at`       Datetime DEFAULT NULL,
- `updated_at`       Datetime DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-INSERT INTO t_user VALUES (1,'tanaka','tanaka@gmail.com');
-INSERT INTO t_user VALUES (2,'kage','kage@gmail.com');
-INSERT INTO t_user VALUES (3,'yamada','yamada@gmail.com');
 
 ---- drop ----
 DROP TABLE IF EXISTS `t_task`;
@@ -31,6 +13,8 @@ create table IF not exists `t_task`
  `updated_at`       Datetime DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-INSERT INTO t_task VALUES (1,'tanaka');
-INSERT INTO t_task VALUES (2,'kage');
-INSERT INTO t_task VALUES (3,'yamada');
+
+---- insert ----
+INSERT INTO t_task VALUES (1,'tanaka', '2019-10-04 15:25:07', '2019-10-04 15:25:07');
+INSERT INTO t_task VALUES (2,'kage', '2020-04-02 15:25:07', '2020-04-04 15:25:07');
+INSERT INTO t_task VALUES (3,'yamada', '2020-05-05 15:25:07', '2020-05-06 15:25:07');
