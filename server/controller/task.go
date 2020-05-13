e34000d6a17544d72f66755f22666e3d0fac7c80
Kage Soichiro <k4gepedia@gmail.com>
2020-05-14 00:18:27 +0900

    sample code

--- server/controller/task.go
+++ server/controller/task.go
@@ -21,24 +21,21 @@ type Task struct {
 	UpdateAt time.Time
 }
 
-type Time struct {
-	UpdateAt time.Time
-}
-
 // サンプル
 func HandlerIndex(w http.ResponseWriter, r *http.Request) {
 	nowTime := time.Now()
 	const format = "2006/01/02 15:04:05"
 	nowTime.Format(format)
-	time := Time{nowTime}
-	res, err := json.Marshal(time)
+	db := util.Sqlhandler()
+	defer db.Close()
+	ins, err := db.Prepare("INSERT INTO t_task (task,created_at,updated_at) VALUES (?,?,?)")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	defer ins.Close()
+	ins.Exec("task-2020", nowTime, nowTime)
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(res)
 }
 
 // 構造体に関して勉強（作成）Create まだ
@@ -82,6 +79,17 @@ func HandlerTasks(w http.ResponseWriter, r *http.Request) {
 
 // 構造体に関して勉強（作成）Update　まだ
 func HandlerTasksUpdate(w http.ResponseWriter, r *http.Request) {
+	//update
+	nowTime := time.Now()
+	db := util.Sqlhandler()
+	defer db.Close()
+	upd, err := db.Prepare("UPDATE t_task SET task = ?, updated_at = ? WHERE id = ? ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer upd.Close()
+	upd.Exec("yamatonadeshiko", nowTime, 33)
 }
 
 // 構造体に関して勉強（作成）Delete まだ
--- server/main.go
+++ server/main.go
@@ -4,13 +4,13 @@ import (
 	"net/http"
 
 	"github.com/kagepedia/go-api/controller"
-	"github.com/kagepedia/go-api/util"
 )
 
 func main() {
-	util.Sqlhandler()
+	// util.Sqlhandler()
 	http.HandleFunc("/", controller.HandlerIndex)
 	http.HandleFunc("/tasks", controller.HandlerTasks)
+	http.HandleFunc("/update", controller.HandlerTasksUpdate)
 	http.HandleFunc("/taskdelete", controller.HandlerTasksDelete)
 	http.ListenAndServe(":8888", nil)
 }
